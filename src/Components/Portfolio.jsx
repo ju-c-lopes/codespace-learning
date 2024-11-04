b926b8c22a0814dc7c0400146fd1805ed6444ecb
Juliano Lopes <juliano.co.lopes@gmail.com>
2024-11-04 17:24:02 +0000

    solve type on image file

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/workdesk.jpeg";
+import image from "../images/workdesk.jpg";
 
 const imageAltText = "desktop with work tools like a laptop, a book and a light bulb";
 
